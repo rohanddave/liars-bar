10dc3183248cbe1e7c838fcb0079810b7a49dc5d
amliang1 <127717015+amliang1@users.noreply.github.com>
2025-08-05 23:00:05 -0700

    Revert "view framework"
    This reverts commit 15cb8d2b007af1a178d835f25380b9210a22e0c9.

--- src/model/view/TableView.java
+++ src/model/view/TableView.java
@@ -1,62 +1,14 @@
-import model.view.table.TableDimensions;
-import model.view.table.TableSide;
-
-public class TableView {
-    private final TableDimensions dimensions;
-    
-    public TableView() {
-        this.dimensions = new TableDimensions();
-    }
-    
+public class TableView{
     public void renderCentralTable(int startRow, int startCol, GameState state) {
-        // Draw table border using dimensions
-        drawTableBorder(startRow, startCol);
-        
-        // Central claim displayer
-        // Revolver if applicable
-        // Last action summary
+        // centrail claime displayer
+        // revolver if applicable
+        //last action summ
     }
-    
-    private void drawTableBorder(int startRow, int startCol) {
-        int width = dimensions.getWidth();
-        int height = dimensions.getHeight();
-        int centerWidth = dimensions.getCenterWidth();
-        int centerHeight = dimensions.getCenterHeight();
-        
-        // Calculate border positions
-        int leftBorder = startCol + (width - centerWidth) / 2;
-        int rightBorder = leftBorder + centerWidth;
-        int topBorder = startRow + (height - centerHeight) / 2;
-        int bottomBorder = topBorder + centerHeight;
-        
-        // Draw horizontal borders (top and bottom)
-        drawHorizontalBorder(topBorder, leftBorder, rightBorder);
-        drawHorizontalBorder(bottomBorder, leftBorder, rightBorder);
-        
-        // Draw vertical borders (left and right)
-        drawVerticalBorder(leftBorder, topBorder, bottomBorder);
-        drawVerticalBorder(rightBorder, topBorder, bottomBorder);
-    }
-    
-    private void drawHorizontalBorder(int row, int startCol, int endCol) {
-        // Draw a horizontal border line using ASCII characters
-        // Implementation will be added in task 3
-    }
-    
-    private void drawVerticalBorder(int col, int startRow, int endRow) {
-        // Draw a vertical border line using ASCII characters
-        // Implementation will be added in task 3
-    }
-    
-    public void renderCurrentClaim(Claim claim) {
+    public void renderCurrentClaim(Claim claim){
         // ┌─────────┐
         // │ CURRENT │
         // │ CLAIM:  │
         // │   K x2  │
         // └─────────┘
-    }
-    
-    public TableDimensions getTableDimensions() {
-        return dimensions;
     }
 }
